3d63d505a92491d5dec30843e642abb8a06e1294
Bryan-Elliott Tam <bryanelliott.tam@ugent.be>
2023-03-27 12:59:49 +0200

    unit test added for the graph constructor and unit test changed in relation to the new struct.

--- test/TREE-graph/graph_test.go
+++ test/TREE-graph/graph_test.go
@@ -2,26 +2,30 @@ package treegraph_test
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 	treegraph "tree-document-topology-visualizer/TREE-graph"
+	"tree-document-topology-visualizer/communication"
 )
 
 func TestCreatingNNodeShouldHaveDifferentId(t *testing.T) {
 	treegraph.ResetNodeCounter()
 	url := ""
 	for i := uint(0); i < 100; i++ {
-		if treegraph.NewNode(url).Id() != fmt.Sprintf("n%v", i) {
-			t.Fatalf("the node should have always a new id.\n The node has id {%v} and the id should be {n%v}", treegraph.NewNode(url).Id(), i)
+		node := treegraph.Node{Url: url}
+		node.BuildId()
+		if node.Id() != fmt.Sprintf("n%v", i) {
+			t.Fatalf("the node should have always a new id.\n The node has id {%v} and the id should be {n%v}", treegraph.Node{Url: url}.Id(), i)
 		}
 	}
 }
 
 func TestAUrlShouldReturnTheRightUrl(t *testing.T) {
 	url := "foo"
-	node := treegraph.NewNode(url)
+	node := treegraph.Node{Url: url}
 
-	if node.Url() != url {
-		t.Fatalf("The URL should be the same. The node has {%v} but it should be {%v}", node.Url(), url)
+	if node.Url != url {
+		t.Fatalf("The URL should be the same. The node has {%v} but it should be {%v}", node.Url, url)
 	}
 }
 
@@ -29,7 +33,7 @@ func TestTheIdOfANodeShouldBeFormated(t *testing.T) {
 	treegraph.ResetNodeCounter()
 	url := "foo"
 	correctId := "n0"
-	node := treegraph.NewNode(url)
+	node := treegraph.Node{Url: url}
 
 	if node.Id() != correctId {
 		t.Fatalf("The id the of the node should be {%v} but it is {%v}", correctId, node.Id())
@@ -37,7 +41,7 @@ func TestTheIdOfANodeShouldBeFormated(t *testing.T) {
 }
 
 func TestTheOperatorOfARelationShouldBeConvertable(t *testing.T) {
-	aNode := treegraph.NewNode("")
+	aNode := treegraph.Node{Url: ""}
 	rawLiteral := "foo"
 	destination := aNode
 	for k, v := range *treegraph.OperatorMapping() {
@@ -59,7 +63,7 @@ func TestTheRelationShouldPanicIfItWasGivenAFaltyOperator(t *testing.T) {
 		}
 	}()
 
-	aNode := treegraph.NewNode("")
+	aNode := treegraph.Node{Url: ""}
 	rawLiteral := "foo"
 	destination := aNode
 
@@ -72,7 +76,7 @@ func TestTheRelationShouldPanicIfItWasGivenAFaltyOperator(t *testing.T) {
 }
 
 func TestARelationShouldBeAbleToConvertLiteralBetweenGuillemet(t *testing.T) {
-	aNode := treegraph.NewNode("")
+	aNode := treegraph.Node{Url: ""}
 	rawLiteral := "\"abc\""
 	destination := aNode
 
@@ -91,7 +95,7 @@ func TestARelationShouldPanicWhenTheLiteralIsHaveOnlyAGuillemetToTheRight(t *tes
 		}
 	}()
 
-	aNode := treegraph.NewNode("")
+	aNode := treegraph.Node{Url: ""}
 	rawLiteral := "\"abc"
 	destination := aNode
 
@@ -110,7 +114,7 @@ func TestARelationShouldPanicWhenTheLiteralIsHaveOnlyAGuillemetToTheLeft(t *test
 		}
 	}()
 
-	aNode := treegraph.NewNode("")
+	aNode := treegraph.Node{Url: ""}
 	rawLiteral := "abc\""
 	destination := aNode
 
@@ -123,7 +127,7 @@ func TestARelationShouldPanicWhenTheLiteralIsHaveOnlyAGuillemetToTheLeft(t *test
 }
 
 func TestARelationShouldReturnAnEquationOfTheRightFormat(t *testing.T) {
-	aNode := treegraph.NewNode("")
+	aNode := treegraph.Node{Url: ""}
 	rawLiteral := "\"2\""
 	destination := aNode
 	correctEquation := "x > 2"
@@ -146,7 +150,7 @@ func TestARelationShouldPanicIfTheOperatorIsNotValid(t *testing.T) {
 		}
 	}()
 
-	aNode := treegraph.NewNode("")
+	aNode := treegraph.Node{Url: ""}
 	rawLiteral := "\"2\""
 	destination := aNode
 	correctEquation := "x > 2"
@@ -169,7 +173,7 @@ func TestARelationShouldPanicIfTheLiteralIsNotValid(t *testing.T) {
 		}
 	}()
 
-	aNode := treegraph.NewNode("")
+	aNode := treegraph.Node{Url: ""}
 	rawLiteral := "\"2"
 	destination := aNode
 	correctEquation := "x > 2"
@@ -183,4 +187,38 @@ func TestARelationShouldPanicIfTheLiteralIsNotValid(t *testing.T) {
 	if relation.Equation() != correctEquation {
 		t.Fatalf("The equation should be {%v} but it is {%v}", correctEquation, relation.Equation())
 	}
+}
+
+func TestCreateNewGraph(t *testing.T) {
+	rawOperator := "https://w3id.org/tree#GreaterThanRelation"
+	value := "\"2022-08-07T08:08:21.000Z\"^^<http://www.w3.org/2001/XMLSchema#dateTime>"
+
+	outputs := []communication.SparqlRelationOutput{
+		{
+			Operator: rawOperator,
+			Value:    value,
+			NextNode: "foo",
+			Node:     "bar",
+		},
+		{
+			Operator: rawOperator,
+			Value:    value,
+			NextNode: "foo2",
+			Node:     "bar2",
+		},
+	}
+
+	expectedGraph := treegraph.Graph{
+		treegraph.Node{Url: "foo"}:  []treegraph.Relation{},
+		treegraph.Node{Url: "bar"}:  []treegraph.Relation{{RawOperator: rawOperator, RawLiteral: value, Destination: treegraph.Node{Url: "foo"}}},
+		treegraph.Node{Url: "foo2"}: []treegraph.Relation{},
+		treegraph.Node{Url: "bar2"}: []treegraph.Relation{{RawOperator: rawOperator, RawLiteral: value, Destination: treegraph.Node{Url: "foo2"}}},
+	}
+
+	resp := treegraph.NewGraphFromSparlRelationOutputs(outputs)
+
+	if !reflect.DeepEqual(resp, expectedGraph) {
+		t.Fatalf("The two graph should be the same.\nThe expected graph is {%v}\nThe returned graph is {%v}", expectedGraph, resp)
+	}
+
 }
