41d4a41de9df9578b7fecd6ac9f6f84a386a4986
Bryan-Elliott Tam <bryanelliott.tam@ugent.be>
2023-03-29 18:07:35 +0200

    small modification of the information outputed by the program

--- main.go
+++ main.go
@@ -16,8 +16,11 @@ var limit uint
 
 func main() {
 	cliParseCliArgs()
-
-	fmt.Printf("Starting to get the relation with a limit of %v using the data source {%v}\n", limit, treeDocumentUrl)
+	if limit == math.MaxUint {
+		fmt.Printf("Starting to get the relation in the data source {%v}\n", treeDocumentUrl)
+	} else {
+		fmt.Printf("Starting to get the relation with a limit of %v in the data source {%v}\n", limit, treeDocumentUrl)
+	}
 	queryOutput, err := communication.GetTreeRelation(treeDocumentUrl, limit)
 	if err != nil {
 		log.Fatal(err)
@@ -35,13 +38,10 @@ func main() {
 
 func cliParseCliArgs() {
 	flag.StringVar(&treeDocumentUrl, "t", "http://localhost:3000/ldes/test", "URL of the TREE document")
-	flag.StringVar(&treeDocumentUrl, "tree-document-url", "http://localhost:3000/ldes/test", "URL of the TREE document")
 
 	flag.StringVar(&graphPath, "p", "./generated/graph.svg", "Resulting path of the graph")
-	flag.StringVar(&graphPath, "path", "./generated/graph.svg", "Resulting path of the graph")
 
 	flag.UintVar(&limit, "l", math.MaxUint, "The maximum number of relation")
-	flag.UintVar(&limit, "limit", math.MaxUint, "The maximum number of relation")
 	flag.Parse()
 
 }
